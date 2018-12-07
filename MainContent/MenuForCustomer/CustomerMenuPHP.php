340cce533d5d8775ea737382abe253e8f47375f8
Daniel <CraZZyB0ndor@users.noreply.github.com>
2018-12-07 23:42:54 +0200

    Have been created structure for customer main memu

---
+++ MainContent/MenuForCustomer/CustomerMenuPHP.php
@@ -0,0 +1,68 @@
+<?php
+
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="StyleForCustomerMenu.css">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Головне меню (НАША ПОШТА)</title>
+</head>
+<body>
+
+<main>
+
+
+    <div id="ProfileClient">
+
+1
+
+    </div>
+
+
+
+
+    <div id="Archive">
+
+        <div id="ArchiveSent">
+
+            2
+
+        </div>
+
+        <div id="ArchiveReceived">
+
+            3
+
+        </div>
+
+    </div>
+
+
+    <div id="SendANDHelp">
+
+        <div id="SendPost">
+
+            4
+
+        </div>
+
+        <div id="OnlineHelp">
+
+            5
+
+        </div>
+
+    </div>
+
+
+
+
+</main>
+
+</body>
+</html>
